6488e7a992acadc0f4625730903a428860a4bcd7
lime-de-rhyme <79634526+lime-de-rhyme@users.noreply.github.com>
2021-06-24 23:39:00 +0900

    Add functions related to HhI

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ DISCORD_TOKEN = settings.DISCORD_TOKEN
 # CREATES A NEW BOT OBJECT WITH A SPECIFIED PREFIX. IT CAN BE WHATEVER YOU WANT IT TO BE.
 bot = commands.Bot(command_prefix="$")
 
+# when $rank hhi
+def rank_of_HhI():
+    HhI = ["HhI★", "HhI☆"]
+    for hhi in HhI:
+        for team_list in trt.values():
+            for tl in team_list:
+                if tl == hhi:
+                    keys = [k for k, v in trt.items() if v == team_list]
+                    HhI += keys   # HhI[2] = HhI★`s rank, HhI[3] = HhI☆'s rank
+                    break
+    return HhI[2], HhI[3]
+
 @bot.command()
 async def rank(ctx, team_name):
     for k, v in trt.items():
@@ -15,7 +27,13 @@ async def rank(ctx, team_name):
             if tn.lower() == team_name.lower():
                 await ctx.channel.send(f"RANK: {k}")
                 return
-    await ctx.channel.send(f"{team_name} is not found.")
+    if team_name.lower() == "hhi":
+	black_star, white_star =  rank_of_HhI()
+	await ctx.channel.send(f"{team_name} has two divisions.\nHhI★ is D rank, HhI☆ is G rank.")
+	return
+    else:
+        await ctx.channel.send(f"{team_name} is not found.")
+	return
             
 	
     
